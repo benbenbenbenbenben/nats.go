87abe29375804b65d8aecf997e1695c9e01166e8
Benjamin Babik <work@benjaminbabik.com>
2025-08-21 19:30:34 +0000

    [FIXED] Adjust formatting in cov.sh for consistency

--- scripts/cov.sh
+++ scripts/cov.sh
@@ -9,12 +9,12 @@ go test -modfile=go_test.mod --failfast -vet=off -v -covermode=atomic -coverprof
 go test -modfile=go_test.mod --failfast -vet=off -v -covermode=atomic -coverprofile=./cov/service.out -coverpkg=github.com/nats-io/nats.go/micro ./micro/...
 go test -modfile=go_test.mod --failfast -vet=off -v -covermode=atomic -coverprofile=./cov/builtin.out -coverpkg=github.com/nats-io/nats.go/encoders/builtin ./test -run EncBuiltin -tags=skip_no_race_tests
 go test -modfile=go_test.mod --failfast -vet=off -v -covermode=atomic -coverprofile=./cov/protobuf.out -coverpkg=github.com/nats-io/nats.go/encoders/protobuf ./test -run EncProto -tags=skip_no_race_tests
-gocovmerge ./cov/*.out > acc.out
+gocovmerge ./cov/*.out >acc.out
 rm -rf ./cov
 
 # Without argument, launch browser results. We are going to push to coveralls only
 # from ci.yml and after success of the build (and result of pushing will not affect
 # build result).
 if [[ $1 == "" ]]; then
-    go tool cover -html=acc.out
+	go tool cover -html=acc.out
 fi
